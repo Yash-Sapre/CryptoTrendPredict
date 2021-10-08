6936a35e607fbf30346dc387ea55cd8d2654df6f
Yash-Sapre <yss052001@gmail.com>
2021-10-08 21:13:18 +0530

    Flask basic setup

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/currency_details/<int:id>')
+def currency_details(id):
+    return render_template('details.html')
