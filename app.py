81b8a5ba343364a76d0de49429af0a6497ae1222
Rahul Agrawal <agrawal.rahul20022@gmail.com>
2021-10-09 16:31:21 +0530

    added doge model

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,Response
-from  fetch_pred import get_pred_eth,get_pred_btc
+from  fetch_pred import get_pred_eth,get_pred_btc ,get_pred_doge
 import io
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -19,6 +19,10 @@ def currency_details(id):
         fig = get_pred_btc()
         fig.get_figure().savefig('static/btc-pred.png')
         return render_template('details.html' , img =1)
+    elif id==2:
+        fig = get_pred_doge()
+        fig.get_figure().savefig('static/doge-pred.png')
+        return render_template('details.html' , img =2)
     elif id==3:
         fig =get_pred_eth()
         fig.get_figure().savefig('static/eth-pred.png')
